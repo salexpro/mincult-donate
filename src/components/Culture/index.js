0ac7acde9bfdfc6673acf2d92f10ad5f31ae4642
Alex <dev@salex.pro>
2023-07-09 14:39:32 -0400

    fix(deps): update swiper imports

--- src/components/Culture/index.js
+++ src/components/Culture/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import { Container } from 'react-bootstrap'
-import { Autoplay, Pagination, Navigation } from 'swiper'
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import cn from 'classnames'
